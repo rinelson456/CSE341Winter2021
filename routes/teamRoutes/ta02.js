d44feb7ee6109243bb8fc0321d228dc172909ac7
rinelson456 <nel15031@byui.edu>
2021-01-14 09:02:51 -0700

    ta02

--- routes/teamRoutes/ta02.js
+++ routes/teamRoutes/ta02.js
@@ -1,15 +1,41 @@
 //TA02 PLACEHOLDER
 // Remember, you can make more of these placeholders yourself! 
 const express = require('express');
-const router = express.Router();
-
+const router = express.Router();​
+const userList = ['Anakin', 'Emily', 'Clifford the big red dog'];
+let userRemoved = false;
+let userFound = false;​
+router.post('/addUser', (req, res, next) => {
+    userRemoved = false;​
+    const foundItemIndex = userList.indexOf(req.body.addUser);
+    if (foundItemIndex >= 0) {
+        userFound = true;
+    } else {
+        userFound = false;
+        userList.push(req.body.addUser);
+    }
+    res.redirect('/ta02');
+});​
+router.post('/removeUser', (req, res, next) => {
+    userFound = false;
+    const foundItemIndex = userList.indexOf(req.body.removeUser);
+    if (foundItemIndex >= 0) {
+        userList.splice(foundItemIndex, 1);
+        userRemoved = false;
+    } else {
+        userRemoved = true;
+    }
+    res.redirect('/ta02');
+});​
 router.get('/', (req, res, next) => {
-    res.render('pages/teamActivities/ta02', {
+    res.render('pages/ta02', {
         title: 'Team Activity 02',
+        userList: userList,
+        userRemoved: userRemoved,
+        userFound: userFound,
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
     });
-});
-
+});​
 module.exports = router;
